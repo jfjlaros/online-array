5155bc54452bd4540d3db9ebda67cc720cdda5d2
J.F.J. Laros <j.f.j.laros@lumc.nl>
2014-08-13 21:54:23 +0200

    Made recursive sub_matrix retrieval.
    Now matrices of all dimensions are supported.

--- online_matrix/online_matrix.py
+++ online_matrix/online_matrix.py
@@ -7,28 +7,30 @@ class OnlineMatrix(numpy.ndarray):
         if type(index) == tuple:
             return self.function(*index)
 
-        if self.dimensions == 1:
-            return self.function(*self.parameters + (index, ))
-        else:
-            sub_matrix = OnlineMatrix([])
+        if self.dimensions > 1:
+            sub_matrix = OnlineMatrix(self.shape[1:])
             sub_matrix.function = self.function
             sub_matrix.parameters = self.parameters + (index, )
             sub_matrix.dimensions = self.dimensions - 1
+
             return sub_matrix
+        #if
+        else:
+            return self.function(*self.parameters + (index, ))
     #__getitem__
 
     def __str__(self):
-        return "{} contains no data".format(str(self.__class__))
+        return "{} contains no data".format(self.__repr__())
 
     def __repr__(self):
-        return self.__str__()
+        return str(self.__class__)
 #OnlineMatrix
 
-def online_matrix(function, dimensions=2, shape=()):
+def online_matrix(function, shape=()):
     matrix = OnlineMatrix(shape)
     matrix.function = function
     matrix.parameters = ()
-    matrix.dimensions = dimensions
+    matrix.dimensions = function.func_code.co_argcount
 
     return matrix
 #online_matrix
--- online_matrix/test.py
+++ online_matrix/test.py
@@ -5,22 +5,23 @@ import online_matrix
 def f(x, y):
     return x + y + 1
 
-def test_matrix(matrix, function, i, j):
-    print "matrix[{}][{}] returns {}, should be {}".format(i, j, matrix[i][j],
-        function(i, j))
-    print "matrix[{}, {}] returns {}, should be {}".format(i, j, matrix[i, j],
-        function(i, j))
-#test_matrix
+def g(x, y, z):
+    return x * y + z + 1
 
 def main():
-    matrix = online_matrix.online_matrix(f, 2, (4, 3))
+    matrix = online_matrix.online_matrix(f, (4, 3))
 
-    for i in range(3):
-        for j in range(4):
-            test_matrix(matrix, f, i, j)
-    print matrix.shape
-    print matrix
-    print repr(matrix)
+    assert(matrix[2][2] == 5)
+    assert(matrix[2, 2] == 5)
+    assert(matrix.shape == (4, 3))
+    assert(matrix.dimensions == 2)
+
+    matrix = online_matrix.online_matrix(g, (4, 3, 5))
+
+    assert(matrix[2][2][1] == 6)
+    assert(matrix[2, 2, 1] == 6)
+    assert(matrix.shape == (4, 3, 5))
+    assert(matrix.dimensions == 3)
 #main
 
 if __name__ == "__main__":
