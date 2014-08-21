8028ea68c0b49f1b8858eec0c8ba178e07aa51a3
J.F.J. Laros <j.f.j.laros@lumc.nl>
2014-08-21 20:45:47 +0200

    Made real mirror arrays for testing.

--- tests/test_online_array.py
+++ tests/test_online_array.py
@@ -2,20 +2,75 @@
 Tests for the `online_array` module.
 """
 
+import numpy
+
 from online_array import online_array
 
 class TestOnlineArray(object):
+    def _f(self, x):
+        return x
+
+    def _g(self, x, y):
+        return x + y + 1
+
+    def _h(self, x, y, z):
+        return x * y + z + 1
+
+    def _fill_array(self, real_array, function, index=(), depth=0):
+        """
+        Fill a real array with the values specified by {function}.
+
+        :arg real_array: A real numpy array.
+        :type real_array: ndarray
+        :arg function: Function that specifies the values.
+        :type function: function
+        :arg index: Index up to {depth} number of dimensions.
+        :type index: tuple(int)
+        :arg depth: Depth of the recursion.
+        :type depth: int
+        """
+        for i in range(real_array.shape[depth]):
+            if real_array.ndim > depth + 1:
+                self._fill_array(real_array, function, index=index + (i, ),
+                    depth=depth + 1)
+            else:
+                real_array[(index) + (i, )] = function(*(index) + (i, ))
+        #for
+    #_fill_array
+
+    def _make_array_pair(self, shape, function):
+        """
+        Make a named pair of arrays (online, real) and add it to the {arrays}
+        dictionary.
+
+        :arg shape: Shape of the arrays.
+        :type shape: tuple(int)
+        :arg function: Function that specifies the values.
+        :type function: function
+
+        :returns: A named pair of arrays.
+        :rtype: dict
+        """
+        array = online_array.OnlineArray(shape, function=function)
+
+        real_array = numpy.ndarray(shape)
+        self._fill_array(real_array, function)
+
+        return {'online': array, 'real': real_array}
+    #_make_array_pair
+
     def setup(self):
         """
         Create some arrays.
         """
-        self.array_1 = online_array.OnlineArray((5, ), function=lambda x: x)
-        self.unbounded_array_1 = online_array.OnlineArray((0, ),
-            function=lambda x: x, unbounded=True)
-        self.array_2 = online_array.OnlineArray((4, 3),
-            function=lambda x, y: x + y + 1)
-        self.array_3 = online_array.OnlineArray((4, 3, 5),
-            function=lambda x, y, z: x * y + z + 1)
+        self.arrays = {}
+
+        self.arrays['1d'] = self._make_array_pair((5, ), self._f)
+        self.arrays['2d'] = self._make_array_pair((4, 3), self._g)
+        self.arrays['3d'] = self._make_array_pair((4, 3, 5), self._h)
+
+        self.unbounded_array = online_array.OnlineArray((0, ),
+            function=self._f, unbounded=True)
     #setup
 
     def _test_boundary(self, array, index):
@@ -57,66 +112,106 @@ class TestOnlineArray(object):
         return index
     #_test_loop
 
+    def _test_content(self, array, mirror, index=(), depth=0):
+        """
+        Test whether the complete content of {array} equals that of {mirror}.
+
+        :arg array: Either an online or a real array.
+        :type array: OnlineArray or ndarray
+        :arg mirror: Either an online or a real array.
+        :type mirror: OnlineArray or ndarray
+        :arg index: Index up to {depth} number of dimensions.
+        :type index: tuple(int)
+        :arg depth: Depth of the recursion.
+        :type depth: int
+        """
+        for i in range(array.shape[depth]):
+            if array.ndim > depth + 1:
+                self._test_content(array, mirror, index=index + (i, ),
+                    depth=depth + 1)
+            else:
+                assert(array[(index) + (i, )] == mirror[(index) + (i, )])
+        #for
+    #_fill_array
+
     def test_index_1(self):
-        assert(self.array_2[2][2] == 5)
+        assert(self.arrays['2d']['online'][2][2] == 5)
 
     def test_index_2(self):
-        assert(self.array_3[2][2][1] == 6)
+        assert(self.arrays['3d']['online'][2][2][1] == 6)
 
     def test_index_3(self):
-        row = self.array_3[0][1]
+        row = self.arrays['3d']['online'][0][1]
 
         assert(row[2] == 3)
     #test_index_3
 
     def test_index_4(self):
-        assert(self.array_1[-2] == self.array_1[3])
+        assert(self.arrays['1d']['online'][-2] ==
+            self.arrays['1d']['online'][3])
 
     def test_numpy_index_1(self):
-        assert(self.array_2[2, 2] == 5)
+        assert(self.arrays['2d']['online'][2, 2] == 5)
 
     def test_numpy_index_2(self):
-        assert(self.array_3[2, 2, 1] == 6)
+        assert(self.arrays['3d']['online'][2, 2, 1] == 6)
 
     def test_numpy_index_3(self):
-        row = self.array_3[0, 1]
+        row = self.arrays['3d']['online'][0, 1]
 
         assert(row[2] == 3)
     #test_index_3
 
     def test_numpy_index_4(self):
-        assert(self.array_2[0, -2] == self.array_2[0, 1])
+        assert(self.arrays['2d']['online'][0, -2] ==
+            self.arrays['2d']['online'][0, 1])
 
     def test_boundary_1(self):
-        assert(self.array_1[4] == 4)
+        assert(self.arrays['1d']['online'][4] == 4)
 
     def test_boundary_2(self):
-        assert(self.array_1[-5] == 0)
+        assert(self.arrays['1d']['online'][-5] == 0)
 
     def test_boundary_3(self):
-        self._test_boundary(self.array_1, 5)
+        self._test_boundary(self.arrays['1d']['online'], 5)
 
     def test_boundary_4(self):
-        self._test_boundary(self.array_1, -6)
+        self._test_boundary(self.arrays['1d']['online'], -6)
 
     def test_unbounded_1(self):
-        assert(self.unbounded_array_1[10] == 10)
+        assert(self.unbounded_array[10] == 10)
 
     def test_unbounded_2(self):
-        assert(self.unbounded_array_1[-10] == -10)
+        assert(self.unbounded_array[-10] == -10)
 
     def test_loop(self):
-        assert(self._test_loop(self.array_1, 10) == 5)
+        assert(self._test_loop(self.arrays['1d']['online'], 10) == 5)
 
     def test_unbounded_loop(self):
-        assert(self._test_loop(self.unbounded_array_1, 10) == 9)
+        assert(self._test_loop(self.unbounded_array, 10) == 9)
 
     def test_assignment(self):
         try:
-            self.array_1[0] = 1
+            self.arrays['1d']['online'][0] = 1
         except TypeError:
             pass
         else:
             raise TypeError("assignment check failed")
     #test_assignment
+
+    def test_content_1(self):
+        self._test_content(self.arrays['1d']['online'],
+            self.arrays['1d']['real'])
+
+    def test_content_2(self):
+        self._test_content(self.arrays['1d']['real'],
+            self.arrays['1d']['online'])
+
+    def test_content_3(self):
+        self._test_content(self.arrays['3d']['online'],
+            self.arrays['3d']['real'])
+
+    def test_content_4(self):
+        self._test_content(self.arrays['3d']['real'],
+            self.arrays['3d']['online'])
 #TestOnlineArray
