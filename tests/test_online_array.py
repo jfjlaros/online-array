e5fda808dd9bdc3d87825aa6058b401bce0e8f24
J.F.J. Laros <j.f.j.laros@lumc.nl>
2014-08-23 19:48:05 +0200

    Variable renaming in testset.

--- tests/test_online_array.py
+++ tests/test_online_array.py
@@ -44,9 +44,9 @@ class TestOnlineArray(object):
         """
         self.arrays = {}
 
-        self.arrays['1d'] = self._make_array_pair((5, ), self._f)
-        self.arrays['2d'] = self._make_array_pair((4, 3), self._g)
-        self.arrays['3d'] = self._make_array_pair((4, 3, 5), self._h)
+        self.array_1 = self._make_array_pair((5, ), self._f)
+        self.array_2 = self._make_array_pair((4, 3), self._g)
+        self.array_3 = self._make_array_pair((4, 3, 5), self._h)
 
         self.unbounded_array = online_array.OnlineArray((0, ),
             function=self._f, unbounded=True)
@@ -114,48 +114,46 @@ class TestOnlineArray(object):
     #_fill_array
 
     def test_index_1(self):
-        assert(self.arrays['2d']['online'][2][2] == 5)
+        assert(self.array_2['online'][2][2] == 5)
 
     def test_index_2(self):
-        assert(self.arrays['3d']['online'][2][2][1] == 6)
+        assert(self.array_3['online'][2][2][1] == 6)
 
     def test_index_3(self):
-        row = self.arrays['3d']['online'][0][1]
+        row = self.array_3['online'][0][1]
 
         assert(row[2] == 3)
     #test_index_3
 
     def test_index_4(self):
-        assert(self.arrays['1d']['online'][-2] ==
-            self.arrays['1d']['online'][3])
+        assert(self.array_1['online'][-2] == self.array_1['online'][3])
 
     def test_numpy_index_1(self):
-        assert(self.arrays['2d']['online'][2, 2] == 5)
+        assert(self.array_2['online'][2, 2] == 5)
 
     def test_numpy_index_2(self):
-        assert(self.arrays['3d']['online'][2, 2, 1] == 6)
+        assert(self.array_3['online'][2, 2, 1] == 6)
 
     def test_numpy_index_3(self):
-        row = self.arrays['3d']['online'][0, 1]
+        row = self.array_3['online'][0, 1]
 
         assert(row[2] == 3)
     #test_index_3
 
     def test_numpy_index_4(self):
-        assert(self.arrays['2d']['online'][0, -2] ==
-            self.arrays['2d']['online'][0, 1])
+        assert(self.array_2['online'][0, -2] == self.array_2['online'][0, 1])
 
     def test_boundary_1(self):
-        assert(self.arrays['1d']['online'][4] == 4)
+        assert(self.array_1['online'][4] == 4)
 
     def test_boundary_2(self):
-        assert(self.arrays['1d']['online'][-5] == 0)
+        assert(self.array_1['online'][-5] == 0)
 
     def test_boundary_3(self):
-        self._test_boundary(self.arrays['1d']['online'], 5)
+        self._test_boundary(self.array_1['online'], 5)
 
     def test_boundary_4(self):
-        self._test_boundary(self.arrays['1d']['online'], -6)
+        self._test_boundary(self.array_1['online'], -6)
 
     def test_unbounded_1(self):
         assert(self.unbounded_array[10] == 10)
@@ -164,14 +162,14 @@ class TestOnlineArray(object):
         assert(self.unbounded_array[-10] == -10)
 
     def test_loop(self):
-        assert(self._test_loop(self.arrays['1d']['online'], 10) == 5)
+        assert(self._test_loop(self.array_1['online'], 10) == 5)
 
     def test_unbounded_loop(self):
         assert(self._test_loop(self.unbounded_array, 10) == 9)
 
     def test_assignment(self):
         try:
-            self.arrays['1d']['online'][0] = 1
+            self.array_1['online'][0] = 1
         except TypeError:
             pass
         else:
@@ -179,67 +177,61 @@ class TestOnlineArray(object):
     #test_assignment
 
     def test_content_1(self):
-        self._test_content(self.arrays['1d']['online'],
-            self.arrays['1d']['real'])
+        self._test_content(self.array_1['online'], self.array_1['real'])
 
     def test_content_2(self):
-        self._test_content(self.arrays['1d']['real'],
-            self.arrays['1d']['online'])
+        self._test_content(self.array_1['real'], self.array_1['online'])
 
     def test_content_3(self):
-        self._test_content(self.arrays['3d']['online'],
-            self.arrays['3d']['real'])
+        self._test_content(self.array_3['online'], self.array_3['real'])
 
     def test_content_4(self):
-        self._test_content(self.arrays['3d']['real'],
-            self.arrays['3d']['online'])
+        self._test_content(self.array_3['real'], self.array_3['online'])
 
     def test_slicing_1(self):
-        assert(self.arrays['1d']['online'][0:][0] ==
-            self.arrays['1d']['online'][0])
+        assert(self.array_1['online'][0:][0] == self.array_1['online'][0])
 
     def test_slicing_2(self):
-        assert(self.arrays['1d']['online'][2:][0] ==
-            self.arrays['1d']['online'][2])
+        assert(self.array_1['online'][2:][0] == self.array_1['online'][2])
 
     def test_slicing_3(self):
-        self._test_content(self.arrays['1d']['online'][2:],
-            self.arrays['1d']['real'][2:])
+        self._test_content(self.array_1['online'][2:],
+            self.array_1['real'][2:])
 
     def test_slicing_4(self):
-        self._test_content(self.arrays['1d']['online'][2:],
-            self.arrays['1d']['real'][2:])
+        self._test_content(self.array_1['online'][2:],
+            self.array_1['real'][2:])
 
     def test_slicing_5(self):
-        self._test_content(self.arrays['1d']['online'][1:][1:],
-            self.arrays['1d']['real'][1:][1:])
+        self._test_content(self.array_1['online'][1:][1:],
+            self.array_1['real'][1:][1:])
 
     def test_slicing_6(self):
-        self._test_content(self.arrays['1d']['online'][::],
-            self.arrays['1d']['real'][::])
+        self._test_content(self.array_1['online'][::],
+            self.array_1['real'][::])
 
     #def test_slicing_6(self):
-    #    self._test_content(self.arrays['1d']['online'][1::2][1::2],
-    #        self.arrays['1d']['real'][1::2][1::2])
+    #    self._test_content(self.array_1['online'][1::2][1::2],
+    #        self.array_1['real'][1::2][1::2])
 
     def test_max_1(self):
-        assert(max(self.arrays['1d']['online']) == 4)
+        assert(max(self.array_1['online']) == 4)
 
     def test_max_2(self):
-        assert(self.arrays['1d']['online'].max() == 4)
+        assert(self.array_1['online'].max() == 4)
 
     def test_min_1(self):
-        assert(min(self.arrays['1d']['online']) == 0)
+        assert(min(self.array_1['online']) == 0)
 
     def test_min_2(self):
-        assert(self.arrays['1d']['online'].min() == 0)
+        assert(self.array_1['online'].min() == 0)
 
     def test_sum_1(self):
-        assert(sum(self.arrays['1d']['online']) == 10)
+        assert(sum(self.array_1['online']) == 10)
 
     def test_sum_2(self):
-        assert(self.arrays['1d']['online'].sum() == 10)
+        assert(self.array_1['online'].sum() == 10)
 
     def test_prod(self):
-        assert(self.arrays['1d']['online'][1:].prod() == 24)
+        assert(self.array_1['online'][1:].prod() == 24)
 #TestOnlineArray
